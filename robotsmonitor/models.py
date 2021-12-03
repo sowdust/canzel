c5b9a75296f71aded688beee9a4964efe30799a5
sowdust <sowdust@gmail.com>
2021-12-03 19:49:49 +0100

    fixed

--- robotsmonitor/models.py
+++ robotsmonitor/models.py
@@ -139,14 +139,14 @@ class RobotsEntry(models.Model):
         options.headless = True
         # most of the following are probably unnecessary
         # but now it works on a VPS
-        chromeOptions.add_argument("--no-sandbox") 
-        chromeOptions.add_argument("--disable-setuid-sandbox") 
-        chromeOptions.add_argument("--remote-debugging-port=9222")
-        chromeOptions.add_argument("--disable-dev-shm-using") 
-        chromeOptions.add_argument("--disable-extensions") 
-        chromeOptions.add_argument("--disable-gpu") 
-        chromeOptions.add_argument("start-maximized") 
-        chromeOptions.add_argument("disable-infobars")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-setuid-sandbox")
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_argument("--disable-dev-shm-using")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("start-maximized")
+        options.add_argument("disable-infobars")
         driver = webdriver.Chrome(service=Service(settings.CHROME_WEBDRIVER_PATH), options=options)
         logger.info('Taking screenshot of %s, saving it to %s' % (url, path))
         print('Taking screenshot of %s, saving it to %s' % (url, path))
